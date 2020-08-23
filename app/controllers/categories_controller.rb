de9f85a4a88aa079b586b3e4ec8bb84ba167b20d
wilman orellano <wilmanjunior2001@gmail.com>
2020-08-23 10:51:36 -0500

    se añadieron alertas

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,34 +1,31 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
 
-  # GET /categories
-  # GET /categories.json
+
   def index
     @categories = Category.all
   end
 
-  # GET /categories/1
-  # GET /categories/1.json
+
   def show
   end
 
-  # GET /categories/new
+
   def new
     @category = Category.new
   end
 
-  # GET /categories/1/edit
+
   def edit
   end
 
-  # POST /categories
-  # POST /categories.json
+
   def create
     @category = Category.new(category_params)
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to @category, notice: 'Categoria se a creado exitosamente.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
@@ -37,12 +34,11 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /categories/1
-  # PATCH/PUT /categories/1.json
+
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html { redirect_to @category, notice: 'la categoria se a actualizado exitosamente.' }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit }
@@ -51,23 +47,22 @@ class CategoriesController < ApplicationController
     end
   end
 
-  # DELETE /categories/1
-  # DELETE /categories/1.json
+
   def destroy
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to categories_url, notice: 'la categoria se a eliminado exitosamente' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_category
       @category = Category.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
+
     def category_params
       params.require(:category).permit(:nombre, :descripcion)
     end
