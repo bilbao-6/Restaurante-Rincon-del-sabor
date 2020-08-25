bb53e6de912a171fefebee1bbe3a83287d886a6b
Hector Bilbao <hectorjose4567@gmail.com>
2020-08-25 15:30:54 -0500

    Realize el buscar

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,8 @@
 class Category < ApplicationRecord
   has_many :dishes
+
+    def self.buscador(termino)
+      Category.where("nombre LIKE ?", "%#{termino}%")
+    end
+
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,6 @@
 class Dish < ApplicationRecord
   belongs_to :category
+  def self.buscador(termino)
+    Dish.where("nombre LIKE ?", "%#{termino}%")
+  end
 end
