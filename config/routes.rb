8330fefd31dc0143c239c5733ee4e175304e29d8
Andres <andresfr0428@gmail.com>
2020-09-06 10:31:01 -0500

    Funcionalidad de editar datos de perfil

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  resources :profiles
   resources :clients
   resources :sales
   resources :categories do
